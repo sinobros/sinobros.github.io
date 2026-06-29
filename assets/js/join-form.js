bc2a47c667217791bc5a449ce5e49829c3cc4bc4
robonate <robonate@robonates-Mac-mini.local>
2026-06-28 22:59:34 -0400

    use same form backend for email signup and contact form

--- assets/js/join-form.js
+++ assets/js/join-form.js
@@ -4,7 +4,7 @@ const email = document.querySelector("#email");
 const button = document.querySelector("#button");
 const form = document.querySelector("#form");
 const GOOGLE_FORM_URL =
-  "https://docs.google.com/forms/d/e/1FAIpQLSdfPJXl3QuZf2zoaZudRMLHu-3rbeZ9-BQpFrKeoinQ2cb0qA/formResponse";
+  "https://docs.google.com/forms/d/e/1FAIpQLSdg7KZYCkEEYdBNSsR9Uo838bN_l8wMY0zQ3nOtj0JAYcLIew/formResponse";
 
 const newFormData = (inputs) => {
   const formData = new FormData();
@@ -18,7 +18,9 @@ const handleSubmit = async (event) => {
   event.preventDefault();
 
   const appendedFormData = newFormData({
-    "entry.684158808": email.value,
+    "entry.1316044900": "noname-emailsignuponly",
+    "entry.593239121": email.value,
+    "entry.749902489": "nomessage-emailsignuponly",
   });
 
   try {
